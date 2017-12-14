f45adce90ebd58fa14fad75b921229964c6c6af2
Przemysław Walkowiak <przemyslaw.walkowiak@put.poznan.pl>
2017-12-14 15:59:05 +0100

    Project settings

--- src/config.h
+++ src/config.h
@@ -4,7 +4,11 @@ namespace Config
 {
 constexpr bool constant_seed = true;
 constexpr bool logger_enabled = true;
+#if SIM_DEBUG == 1
+constexpr bool debug_logger_enabled = true;
+#else
 constexpr bool debug_logger_enabled = false;
+#endif
 
 constexpr double epsilon = 0.0001;
 } // namespace Config
