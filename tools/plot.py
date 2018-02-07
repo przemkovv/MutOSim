c6ace56712aefbc4db8c79960ce8c860800d1b8b
Przemysław Walkowiak <przemyslaw.walkowiak@put.poznan.pl>
2018-02-07 13:54:01 +0100

    Plotting relations between scenarios

--- tools/plot.py
+++ tools/plot.py
@@ -5,11 +5,14 @@ Simulator of network with Mutual Overflows.
 
 Usage:
     plot.py <DATA_FILE> [-p PROPERTY]
-            [--linear] [--y_limit=Y_LIMIT]
+            [--linear]
+            [--y_limit_max=Y_LIMIT_MAX]
+            [--y_limit_min=Y_LIMIT_MIN]
             [-x X] [-y Y]
             [--save] [--output-dir=DIR]
             [--quiet]
             [--bp] [-i INDICES]
+            [-r]
     plot.py -h | --help
 
 Arguments:
@@ -20,8 +23,8 @@ Options:
     -p PROPERTY                 property from data file to plot
                                 [default: P_block]
     --linear                    linear plot (default is log)
-    --y_limit=Y_LIMIT           bottom (for log) or top (for linear) limit on
-                                y axis [default: 1e-6]
+    --y_limit_max=Y_LIMIT       top limit on y axis [default: 5]
+    --y_limit_min=Y_LIMIT       bottom limit on y axis [default: 1e-6]
     -x X                        number of plots horizontally [default: 3]
     -y Y                        number of plots vertically [default: 3]
     -s, --save                  save to file
@@ -30,12 +33,14 @@ Options:
                                 [default: data/results/plots/]
     --bp                        enable box plots
     -i INDICES                  indices of scenarios to plot [default: -1]
+    -r, --relatives             plot relations
 
 """
 import os.path as path
 import os
 import json
 import statistics
+from pprint import pprint
 import itertools
 from docopt import docopt
 import matplotlib.pyplot as plt
@@ -97,16 +102,16 @@ def set_plot_style(ax):
     ax.xaxis.set_minor_locator(minorLocator)
 
 
-def set_plot_linear_style(ax, top=15e5):
+def set_plot_linear_style(ax, top=15e5, bottom=0):
     set_plot_style(ax)
     ax.set_yscale("linear")
-    ax.set_ylim(bottom=0, top=top, auto=True, emit=True)
+    ax.set_ylim(bottom=bottom, top=top, auto=True, emit=True)
 
 
-def set_plot_log_style(ax, bottom=1e-6):
+def set_plot_log_style(ax, top=5, bottom=1e-6):
     set_plot_style(ax)
     ax.set_yscale("log")
-    ax.set_ylim(bottom=bottom, top=5, auto=True, emit=True)
+    ax.set_ylim(bottom=bottom, top=top, auto=True, emit=True)
 
 
 def get_tcs_served_by_groups(scenario_results):
@@ -125,16 +130,33 @@ def get_tcs_served_by_groups(scenario_results):
     return tcs_served_by_groups
 
 
+def describe_dict(d, prefix=()):
+    for k, v in d.items():
+        path = prefix + (k, )
+        yield path
+        if isinstance(v, dict):
+            yield from describe_dict(v, path)
+
+
+def compare_dicts_structure(d1, d2):
+    #  pprint(sorted(describe_dict(d1)))
+    #  pprint(sorted(describe_dict(d2)))
+    return sorted(describe_dict(d1)) == sorted(describe_dict(d2))
+
+
 def main():
     args = docopt(__doc__, version='0.1')
     data_file = args["<DATA_FILE>"]
     data = json.load(open(data_file))
     stat_name = args["-p"]
-    y_limit = float(args["--y_limit"])
+    y_limit_min = float(args["--y_limit_min"])
+    y_limit_max = float(args["--y_limit_max"])
     logarithmic_plot = not args["--linear"]
     enable_boxplots = args["--bp"]
 
     title = path.splitext(path.basename(data_file))[0] + "_" + stat_name
+    if args['--relatives']:
+        title += "_relatives"
 
     fig = plt.figure(figsize=(32, 18), tight_layout=True)
     fig.canvas.set_window_title(title)
@@ -150,20 +172,24 @@ def main():
         filtered_data = {k: data[k]
                          for k in [sorted(data.keys())[i] for i in indices]}
 
+    all_data = {}
+
     for scenario_file, scenario_results in filtered_data.items():
+        all_data[scenario_file] = {}
         tc_sizes, scenario = load_traffic_classes_sizes(scenario_file)
         print(scenario_file)
-        tc_data_x = []
-        tc_data_y = {}
+        tc_data_x = all_data[scenario_file].setdefault("x", [])
+        tc_data_y = all_data[scenario_file].setdefault("y", {})
         tc_data_y_by_size = {}
         if not logarithmic_plot:
             def set_style(ax):
-                return set_plot_linear_style(ax, y_limit)
+                return set_plot_linear_style(ax, y_limit_max, y_limit_min)
             aggregate = True
-            plots_number_x = 2 * len(data)
+            if not args['--relatives']:
+                plots_number_x = 2 * len(filtered_data)
         else:
             def set_style(ax):
-                return set_plot_log_style(ax, y_limit)
+                return set_plot_log_style(ax, y_limit_max, y_limit_min)
             aggregate = False
 
         markers = ['+', 'x', 's']
@@ -178,45 +204,82 @@ def main():
                 append_tc_stat_for_groups_by_size(
                     tc_data_y_by_size, result, stat_name, tc_sizes)
 
-        for group_name, group_data_y in tc_data_y_by_size.items():
-            markerscycle = itertools.cycle(markers)
-            ax = fig.add_subplot(plots_number_x, plots_number_y, plot_id)
-            for tc_size, data_y in group_data_y.items():
-                ax.plot(tc_data_x, data_y, label="S{}".format(tc_size),
-                        marker=next(markerscycle))
+        if not args['--relatives']:
+            for group_name, group_data_y in tc_data_y_by_size.items():
+                markerscycle = itertools.cycle(markers)
+                ax = fig.add_subplot(plots_number_x, plots_number_y, plot_id)
+                for tc_size, data_y in group_data_y.items():
+                    ax.plot(tc_data_x, data_y, label="S{}".format(tc_size),
+                            marker=next(markerscycle))
 
-            set_style(ax)
-            ax.set_title("{} ({})".format(group_name, scenario["name"]))
-            ax.set_ylabel(stat_name)
-            ax.set_xlabel("a")
-            plot_id += 1
-            ax.legend()
+                set_style(ax)
+                ax.set_title("{} ({})".format(group_name, scenario["name"]))
+                ax.set_ylabel(stat_name)
+                ax.set_xlabel("a")
+                plot_id += 1
+                ax.legend()
 
-        for group_name, group_data_y in tc_data_y.items():
-            markerscycle = itertools.cycle(markers)
-            ax = fig.add_subplot(plots_number_x, plots_number_y, plot_id)
-            for tc_id, data_y in group_data_y.items():
+            for group_name, group_data_y in tc_data_y.items():
+                markerscycle = itertools.cycle(markers)
+                ax = fig.add_subplot(plots_number_x, plots_number_y, plot_id)
+                for tc_id, data_y in group_data_y.items():
 
-                if enable_boxplots:
-                    ax.boxplot(data_y, positions=tc_data_x, notch=False,
-                               widths=0.05, bootstrap=10000, sym='',
-                               vert=True, patch_artist=False,
-                               manage_xticks=False)
+                    if enable_boxplots:
+                        ax.boxplot(data_y, positions=tc_data_x, notch=False,
+                                widths=0.05, bootstrap=10000, sym='',
+                                vert=True, patch_artist=False,
+                                manage_xticks=False)
 
-                ax.plot(tc_data_x,
-                        [statistics.mean(serie) for serie in data_y],
-                        label="TC{} S{}".format(tc_id, tc_sizes[tc_id]),
-                        marker=next(markerscycle))
+                    ax.plot(tc_data_x,
+                            [statistics.mean(serie) for serie in data_y],
+                            label="TC{} S{}".format(tc_id, tc_sizes[tc_id]),
+                            marker=next(markerscycle))
 
-            set_style(ax)
-            ax.set_title("{} V{} ({})"
-                         .format(group_name,
-                                 scenario["groups"][group_name]["capacity"],
-                                 scenario["name"]))
-            ax.set_ylabel(stat_name)
-            ax.set_xlabel("a")
-            plot_id += 1
-            ax.legend()
+                set_style(ax)
+                ax.set_title("{} V{} ({})"
+                            .format(group_name,
+                                    scenario["groups"][group_name]["capacity"],
+                                    scenario["name"]))
+                ax.set_ylabel(stat_name)
+                ax.set_xlabel("a")
+                plot_id += 1
+                ax.legend()
+
+    if args['--relatives']:
+        for k1, k2 in itertools.combinations(all_data.keys(), 2):
+            print((k1, k2))
+            if all_data[k1]['x'] == all_data[k2]['x']:
+                print("OK")
+            if not compare_dicts_structure(all_data[k1]['y'], all_data[k2]['y']):
+                print("NOT OK")
+                continue
+            for group_name, k1_group_data_y in all_data[k1]['y'].items():
+                k2_group_data_y = all_data[k2]['y'][group_name]
+
+                markerscycle = itertools.cycle(markers)
+                ax = fig.add_subplot(plots_number_x, plots_number_y, plot_id)
+                for tc_id, k1_data_y in k1_group_data_y.items():
+                    k2_data_y = k2_group_data_y[tc_id]
+                    k2_data_y_means = [statistics.mean(x) for x in k2_data_y]
+                    k1_data_y_means = [statistics.mean(x) for x in k1_data_y]
+
+                    plot_data = [x/y*100 if y != 0 else 0
+                                for x, y in zip(k1_data_y_means, k2_data_y_means)]
+
+                    ax.plot(tc_data_x,
+                            plot_data,
+                            label="TC{} S{}".format(tc_id, tc_sizes[tc_id]),
+                            marker=next(markerscycle))
+
+                set_style(ax)
+                ax.set_title("{} V{}\n ({} / \n{})"
+                            .format(group_name,
+                                    scenario["groups"][group_name]["capacity"],
+                                    k1, k2))
+                ax.set_ylabel("{}'s ratio [%]".format(stat_name))
+                ax.set_xlabel("a")
+                plot_id += 1
+                ax.legend()
 
     if args["--save"]:
         output_dir = args["--output-dir"]
@@ -225,10 +288,9 @@ def main():
         output_file = title + ".pdf"
         output_file = path.join(output_dir, output_file)
         plt.savefig(output_file)
-    if "--quiet" not in args:
+    if not args["--quiet"]:
         plt.show()
 
 
 if __name__ == "__main__":
     main()
-
