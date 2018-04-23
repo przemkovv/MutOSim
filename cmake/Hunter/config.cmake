f5d6399b5a18dffd833e28a5ae2a5cfb62db404a
Przemysław Walkowiak <przemyslaw.walkowiak@put.poznan.pl>
2018-04-23 13:01:21 +0000

    Merged in devops/hunter (pull request #1)
    Use hunter based dependencies management
    
    * Initial preparation for hunter
    
    * Update hunter
    
    * Add local config for hunter
    
    * Cleanup cmakelists

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,3 @@
+
+# hunter_cmake_args(nlohmann_json CMAKE_ARGS JSON_MultipleHeaders=ON JSON_BuildTests=OFF)
+hunter_config(nlohmann_json VERSION 3.1.2 CMAKE_ARGS JSON_MultipleHeaders=ON JSON_BuildTests=OFF)
